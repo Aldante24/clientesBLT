6be6aa60c2fd860fa09fe8a833c1a61a8ade093f
Aldo Martinez Amador <mtz.aldo24@gmail.com>
2017-08-24 14:26:22 -0500

    Archivos agregados

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+	$server = "localhost";
+	$user = "root";
+	$password = "admin";//poner tu propia contraseña, si tienes una.
+	$bd = "siatBlt";
+
+	$conexion = mysqli_connect($server, $user, $password, $bd);
+	if (!$conexion){
+		die('Error de Conexión: ' . mysqli_connect_errno());
+
+	}
+?>
